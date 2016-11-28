6aa36ab3439ffa70bc1b5e69b99ec93986f2c78e
LuyangSun <Lsun12@winona.edu>
2016-11-28 10:58:42 -0600

    Add files via upload
    Hi Brad, I was also try to add some animation on the side image part, but my styles.css sheet is not work with my .php page, but I am still keep the code in both of them, can you help me to make them become active? Thank You

--- timeline.php
+++ timeline.php
@@ -1,9 +1,7 @@
 <!Doctype html>
 <html lang = 'en'>
 <head>
-<title>
-cswebsite
-</title>
+<title>Alumni</title>
 <meta charset = 'utf-8'>
 <link rel='stylesheet' href='styles.css'>
 <script src='java.js' type='text/javascript'></script>
@@ -31,19 +29,28 @@ cswebsite
 		<div class="times">
 		<ul>
         <li>
-		<b></b><span>2012</span><p>content</p>
+		<b></b><span>current project</span>
+		<p>The name of the project is the <strong>"Sensor Equipped Parking Lot"</strong>, the purpose 
+		of this project is help people use an Iphone application that is integrated with a 
+		sensor - equipped parking lot to find where have the empty place that they can park 
+		their Vechile. For more infroamtion please visit this page <a href="http://cs.winona.edu/CSConference/SeniorSem/Prapti_poster_big.jpg">computer science project</a>. 
+		</p>
 		</li>
-        <li>
-		<b></b><span>2013</span><p>content</p>
+		<li>
+		<b></b><span>additional project</span>
+		<p>The name of this projecy is <strong>"Why But a CPS Clicker When You Can Use Your Laptop - a classroom testing solution for laptop universities"</strong>
+		the purpose of this project is to create a testing system designed for use with laptops to benfit 
+		universities with a laptop program. The system prevents students form being able to cheat while taking exams. And the system allow instructor to create test and students to be able 
+		to submit answer using their laptops.For more infroamtion please visit this page <a href="http://cs.winona.edu/CSConference/SeniorSem/Schaub_poster_big.jpg">computer science project</a>.
+		</p>
 		</li>
-        <li>
-		<b></b><span>2014</span><p>content</p>
-		</li>
-        <li>
-		<b></b><span>2015</span><p>content</p>
-		</li>
-        <li>
-		<b></b><span>2016</span><p>content</p>
+		<li>
+		<b></b><span>In 2010 </span>
+		<p>The name of this project is <strong>"Data Entry On An Iphone Or A Full Keybord"</strong>
+		The purpose of this project following a few step and some ingenuity. a mcuh richer user experience could be seen
+		and create a real time connection between a workstation and iphone, and allow user to use the full keyborad or eliminate the sync process. 
+		For more infroamtion please visit this page <a href="http://cs.winona.edu/styles/Images/ericbusch.jpg">computer science project</a>.
+		</p>
 		</li>
     </ul>
 </div>
@@ -51,7 +58,11 @@ cswebsite
 		
 		<div id = "images" class = "mobile col-m-2 col-l-2">
 			<ul id = "imagelist">
-				<li>IMAGES</li>
+				<li>
+				<div class="tooltip">animation
+				<span class="tooltiptext">text</span>
+				</div>
+				</li>
 				<li>IMAGES</li>
 				<li>IMAGES</li>
 				<li>IMAGES</li>
